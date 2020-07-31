c68bc491afde4860749053a2b8443cdf99ea44de
puthran28 <61663185+puthran28@users.noreply.github.com>
2020-08-01 01:16:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import UserDashBoard from "./user/UserDashBoard.js";
+import UserDashBoard from "./components/UserDashBoard.js";
 import "./styles.css";
-import Calender from "./user/cal.js";
+
 
 function App() {
   return (
     <div className="App">
-      <Calender />
       <UserDashBoard />
     </div>
   );
